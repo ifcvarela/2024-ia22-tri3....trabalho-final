1217d37a08160922e57fea19fc0f8b287a8a587e
gatino <gatinocorp@gmail.com>
2024-10-10 11:40:04 -0300

    ...

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express"
 import { connect } from "../database"
+import bcrypt from "bcrypt"
 
 const listUsers: RequestHandler = async (req, res) => {
   const db = await connect()
@@ -11,7 +12,8 @@ const createUser: RequestHandler = async (req, res) => {
   const db = await connect()
   try {
     const { name, email, password } = req.body
-    const ret = await db.run(`INSERT INTO users (name, email, password) VALUES (?, ?, ?)`, [name, email, password])
+    const encryptedPassword = await bcrypt.hash(password, 10)
+    const ret = await db.run(`INSERT INTO users (name, email, password) VALUES (?, ?, ?)`, [name, email, encryptedPassword])
     res.status(200).json(ret)
   } catch (error) {
     res.status(400).json({ error })
@@ -21,7 +23,8 @@ const createUser: RequestHandler = async (req, res) => {
 const updateUser: RequestHandler = async (req, res) => {
   const db = await connect()
   const { id, name, email, password } = req.body
-  const ret = db.run(`UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?`, [name, email, password, id])
+  const encryptedPassword = await bcrypt.hash(password, 10)
+  const ret = db.run(`UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?`, [name, email, encryptedPassword, id])
   res.status(200).json(ret)
 }
 
